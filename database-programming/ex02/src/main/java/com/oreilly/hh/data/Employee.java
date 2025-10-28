a17ad8fca72a74bcda24a265b67ec59ded7c4ea1
LEE SEUNGHWA <ox_____@naver.com>
2025-10-28 22:37:19 +0900

    Pratice mapping

--- database-programming/ex02/src/main/java/com/oreilly/hh/data/Employee.java
+++ database-programming/ex02/src/main/java/com/oreilly/hh/data/Employee.java
@@ -2,8 +2,16 @@ package com.oreilly.hh.data;
 
 import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Employee  implements java.io.Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
